cffd22ad03b34c368b43f273023e3571f12cf50f
moonsoo20 <98132695+moonsoo20@users.noreply.github.com>
2024-02-03 23:16:41 +0900

    [Silver III] Title: N과 M (2), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+def dfs(n, s, lst):         
+    if n==M:               
+        ans.append(lst)
+        return
+
+    for j in range(s, N+1):
+        dfs(n+1, j+1, lst+[j])
+
+N, M = map(int, input().split())
+ans = []
+
+dfs(0, 1, [])   
+for lst in ans:
+    print(*lst)
