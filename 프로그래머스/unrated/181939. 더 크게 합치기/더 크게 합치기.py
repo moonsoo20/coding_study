fd73493f9574c010c1fd82f4721ed8cfeebf677d
moonsoo20 <98132695+moonsoo20@users.noreply.github.com>
2024-04-03 22:00:01 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    answer = 0
+    str1=str(a)+str(b)
+    str2=str(b)+str(a)
+    
+    if int(str1)>int(str2):
+        answer=int(str1)
+    else:
+        answer=int(str2)
+    
+    return answer
