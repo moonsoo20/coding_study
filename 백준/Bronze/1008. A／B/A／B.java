2680d6e1f6954f9c554f178abf9ae0c03488d407
moonsoohyeon <98132695+moonsoo20@users.noreply.github.com>
2025-05-15 08:59:54 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         
-        int A= sc.nextInt();
-        int B= sc.nextInt();
+        int a= sc.nextInt();
+        int b= sc.nextInt();
         
-        System.out.println((double)A/B);
+        System.out.print((double)a/b);
     }
 }
